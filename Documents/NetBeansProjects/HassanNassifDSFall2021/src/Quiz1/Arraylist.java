fee67a788e8077897ecbace9ec87df135ba87ed7
Hassan-Nassif <72742381+Hassan-Nassif@users.noreply.github.com>
2020-10-25 09:23:16 +0200

    Quiz 1

---
+++ Documents/NetBeansProjects/HassanNassifDSFall2021/src/Quiz1/Arraylist.java
@@ -0,0 +1,20 @@
+package Quiz1;
+
+/**
+ *
+ * @author Hassan Nassif
+ */
+public class Arraylist {
+    int counter = 0;
+    private int effectiveSize;
+    public int countOccurrences(Object element){
+         for(int i=0;i<effectiveSize;i++)
+            if(list[i] == element){
+                counter++;
+            }else{
+        return counter;
+    }
+      
+    
+    
+
---
+++ Documents/NetBeansProjects/HassanNassifDSFall2021/src/Quiz1/TestArraylist.java
@@ -0,0 +1,15 @@
+package Quiz1;
+/**
+ *
+ * @author Hassan Nassif
+ */
+public class TestArraylist {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
